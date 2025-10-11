017fae7f958c02612051b1e16bbec994bebdfd64
Liaqat Paindah <liaqat.paindah@gmail.com>
2025-10-12 00:48:47 +0430

    Professional Account Step 01 UI Added

---
+++ types/auth/register.ts
@@ -0,0 +1,24 @@
+export interface RegisterFormData {
+  username: string;
+  category: string;
+  country: string;
+  streetAddress: string;
+  city: string;
+  region: string;
+  postalCode: string;
+  website?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  password: string;
+  repassword: string;
+  subCategories: string[];
+  services: string[];
+}
+
+export interface RegisterResponse {
+  success: boolean;
+  message: string;
+  userId?: string;
+}
