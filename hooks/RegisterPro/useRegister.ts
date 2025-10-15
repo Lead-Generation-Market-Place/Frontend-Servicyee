38ae5690e5637fde8e99a9ba54853fe82c3754d0
Liaqat Paindah <liaqat.paindah@gmail.com>
2025-10-15 20:07:40 +0430

    Professional Account Step 03 Added

--- hooks/RegisterPro/useRegister.ts
+++ hooks/RegisterPro/useRegister.ts
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { RegisterFormData } from "@/types/auth/register";
-import { registerUser } from "@/app/api/auth/register";
+import {
+  registerUser,
+  UpdateBusinessName,
+} from "@/app/api/services/ProAccount";
 import toast from "react-hot-toast";
+import { useMutation } from "@tanstack/react-query";
 
 export function useRegister() {
   const [isPending, setIsPending] = useState(false);
   const router = useRouter();
 
-
   const register = async (data: RegisterFormData) => {
     setIsPending(true);
     try {
       const response = await registerUser(data);
-      localStorage.setItem("professionalData", JSON.stringify(response.professional));
+      localStorage.setItem(
+        "professionalData",
+        JSON.stringify(response.professional)
+      );
       router.push("/home-services/dashboard/services/step-2");
     } catch (error: unknown) {
       if (error instanceof Error) {
@@ -27,4 +33,21 @@ export function useRegister() {
   };
 
   return { register, isPending };
+}
+
+export function useUpdateBusinessName() {
+  const router = useRouter();
+  return useMutation({
+    mutationKey: ["UpdateBusinessName"],
+    mutationFn: (data: { businessName: string; id: string }) =>
+      UpdateBusinessName(data),
+    onSuccess: () => {
+      router.push(`/home-services/dashboard/services/step-4`);
+    },
+    onError: (error: any) => {
+      toast.error(
+        error?.response?.data?.message || "Failed to update Business Name"
+      );
+    },
+  });
 }
